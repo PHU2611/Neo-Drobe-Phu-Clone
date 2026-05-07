6930e0f3fd6664d287ec9d1b692550593cd2ddca
PHU2611 <85063046+PHU2611@users.noreply.github.com>
2026-05-07 20:25:21 +0700

    Remove outdated function declarations from Webserver.h

--- Official_code/include/Webserver.h
+++ Official_code/include/Webserver.h
@@ -1,8 +1,5 @@
 #pragma once
+
 void initWebServer();
-<<<<<<< Updated upstream:Official_code/include/Webserver.h
-=======
 void startWebServerTask();
 bool fetchLatestWebInputs();
-
->>>>>>> Stashed changes:Official_code/include/net/Webserver.h
