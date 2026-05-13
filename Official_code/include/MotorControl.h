586934f8e395313de2b182da0dcaf7cea20c6095
PHU2611 <85063046+PHU2611@users.noreply.github.com>
2026-05-13 14:44:10 +0700

    Delete Official_code directory

--- Official_code/include/MotorControl.h
+++
@@ -1,8 +0,0 @@
-#pragma once //protection against multiple inclusions in the same file
-#include <ESP32Servo.h>
-
-void initMotors();
-void writeMotors();
-void stopMotors();
-void WaitForKeyStroke();
-void calibratre();
--- Official_code/include/Webserver.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void initWebServer();
-void startWebServerTask();
-bool fetchLatestWebInputs();
